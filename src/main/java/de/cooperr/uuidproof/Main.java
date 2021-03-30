e1d668f7505e3cb391b7361bce0498c661c85b9d
Cooperr06 <colin.rinke@googlemail.com>
2021-03-30 10:23:15 +0200

    Update

--- src/main/java/de/cooperr/uuidproof/Main.java
+++ src/main/java/de/cooperr/uuidproof/Main.java
@@ -1,10 +1,14 @@
 package de.cooperr.uuidproof;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.UUID;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         boolean running = true;
         long count = 0;
@@ -26,12 +30,19 @@ public class Main {
 
                 running = false;
 
+                final File file = new File("src/main/resources/", "Result.txt");
+
+                FileOutputStream outputStream = new FileOutputStream(file);
+                outputStream.write(("Try: " + count + "\nResult: TRUE\nUUID_1: " + firstUUID.toString() + "\nUUID_2: " + secondUUID.toString()).getBytes());
+                outputStream.flush();
+                outputStream.close();
+
             } else {
 
                 System.out.println("Try: " + count);
                 System.out.println("Result: FALSE\n");
                 System.out.println("UUID_1: " + firstUUID.toString());
-                System.out.println("UUID_2: " + secondUUID.toString() + "\n\n----------\n");
+                System.out.println("UUID_2: " + secondUUID.toString() + "\n\n--------------------\n");
 
             }
         }
