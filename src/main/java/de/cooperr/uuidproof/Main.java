a7e3544b1d41f091e1db4654310a27cfc772f0ac
Cooperr06 <colin.rinke@googlemail.com>
2021-03-30 16:24:27 +0200

    Update

--- src/main/java/de/cooperr/uuidproof/Main.java
+++ src/main/java/de/cooperr/uuidproof/Main.java
@@ -1,7 +1,6 @@
 package de.cooperr.uuidproof;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
@@ -20,7 +19,7 @@ public class Main {
             UUID firstUUID = UUID.randomUUID();
             UUID secondUUID = UUID.randomUUID();
 
-            if (firstUUID.toString().equals(secondUUID.toString())) {
+            if (firstUUID.equals(secondUUID)) {
 
                 System.out.println("Try: " + count);
                 System.out.println("Result: TRUE\n");
